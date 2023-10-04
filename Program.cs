0db7d8d952f299bc27e0e5efc52e61f9e7d56c95
BladeCry <129335811+ibrahimyasar0621@users.noreply.github.com>
2023-10-04 20:19:54 +0300

    HACKER RANK ODEVI EKLENDI Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,33 @@ class Program
         sonuc =time>=6 && time<11 ? "günaydın" : time<=18 ? "iyi günler" : "iyigeceler"; //time11 den küçük küçük eşit 11 ise günaydın değilse diğer koşul olan 18eşit yada küçük olma koşulunu çalıştırıyor ve 18den küçükse iyi günler değilse iyi geceler yazacak.
 
         //İKİ YAZIM ARASINDAKİ FARK BİRİSİ UZUN İNCE OLDUĞUNDAN OKUNMASI ANLAŞILMASI ZOR OLUYOR. DİĞERİ SATIR SATIR OLDUĞUNDAN OKUNMASI ANLAŞILMASI DAHA KOLAY
-        Console.WriteLine(sonuc);           
+        Console.WriteLine(sonuc);  
 
     }
 }
+// HACKER RANK ÖDEVİ     
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        if (N % 2 == 1)                                      // EGER TAMSAYIMIZ TEK İSE WİRD YAZDIR
+        {
+            Console.WriteLine("Weird");
+        }
+        else if (N % 2 == 0 && (N >= 2 && N <= 5))          // EGER SAYIMIZ ÇİFT SAYI İSE VE (2 YE EŞİTBÜYÜK VE 5 EŞİTKÜÇÜK İSE NOT WEİRD YAZDIR)
+        {
+            Console.WriteLine("Not Weird");
+        }
+        else if (N % 2 == 0 && (N >=6 && N <= 20))         // EGER SAYIMIZ ÇİFT SAYI İSE VE (6 EŞİTBÜYÜK VE 20 EŞİTKÜÇÜK İSE WEİRD YAZDIR)
+        {
+            Console.WriteLine("Weird");
+        }
+        else if (N % 2 == 0 && N >= 20)                   // EGER SAYIMIZ ÇİFT SAYI İSE VE (20 EŞİTBÜYÜK İSE NOT WEİRD YAZDIR)
+        {
+            Console.WriteLine("Not Weird");
+        }
+    }
+}
+
